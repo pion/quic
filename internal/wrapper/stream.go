166a0ed051e4bde2d3f4b3a41425c2fd8e34778e
Henry <cryptix@riseup.net>
2020-07-01 16:25:51 +0200

    fix stream closing in wrapper

--- internal/wrapper/stream.go
+++ internal/wrapper/stream.go
@@ -1,6 +1,11 @@
 package wrapper
 
 import (
+	"errors"
+	"fmt"
+	"io"
+	"net"
+
 	quic "github.com/lucas-clemente/quic-go"
 )
 
@@ -17,7 +22,20 @@ func (s *Stream) Read(p []byte) (int, error) {
 // ReadQuic reads a frame and determines if it is the final frame
 func (s *Stream) ReadQuic(p []byte) (int, bool, error) {
 	n, err := s.s.Read(p)
-	fin := false // TODO determine if closed
+	fin := false
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			fin = true
+		} else {
+			if ne, ok := err.(net.Error); ok && !ne.Timeout() {
+				fin = true
+			} else {
+				// TODO determine if closed
+				fmt.Println("[quic debug] read error: %T %+v", err, err)
+				// seriously, which error isn't fin=true but timeout?
+			}
+		}
+	}
 	return n, fin, err
 }
 
@@ -28,7 +46,14 @@ func (s *Stream) Write(p []byte, fin bool) (int, error) {
 
 // WriteQuic writes a frame and closes the stream if fin is true
 func (s *Stream) WriteQuic(p []byte, fin bool) (int, error) {
-	return s.s.Write(p) // TODO close stream
+	n, err := s.s.Write(p)
+	if err != nil {
+		return n, err
+	}
+	if fin {
+		return n, s.s.Close()
+	}
+	return n, nil
 }
 
 // StreamID returns the ID of the QuicStream
