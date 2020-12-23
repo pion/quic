4c4f1373f3d95699fba8f4d6f99676d58ac124a4
vrngupta53 <vrngupta53@gmail.com>
2020-12-24 00:54:13 +0530

    Expose SetDeadLine function to BidirectionalStream
    Expose setDeadline function from wrapper to allow different timeout
    times than default

--- bidirectionalstream.go
+++ bidirectionalstream.go
@@ -1,6 +1,8 @@
 package quic
 
 import (
+	"time"
+
 	quic "github.com/lucas-clemente/quic-go"
 	"github.com/pion/quic/internal/wrapper"
 )
@@ -16,7 +18,7 @@ func (s *BidirectionalStream) Write(data StreamWriteParameters) error {
 	return err
 }
 
-// ReadInto reads from the QuicReadableStream into the buffer.
+// ReadInto reads from the stream into the buffer.
 func (s *BidirectionalStream) ReadInto(data []byte) (StreamReadResult, error) {
 	n, fin, err := s.s.ReadQuic(data)
 	return StreamReadResult{
@@ -28,6 +30,11 @@ func (s *BidirectionalStream) ReadInto(data []byte) (StreamReadResult, error) {
 // StreamID returns the ID of the QuicStream
 func (s *BidirectionalStream) StreamID() uint64 {
 	return s.s.StreamID()
+}
+
+// SetDeadline sets read and write deadlines associated with the stream. A zero value for t means Read and Write will not timeout.
+func (s *BidirectionalStream) SetDeadline(t time.Time) error {
+	return s.s.SetDeadline(t)
 }
 
 // Detach detaches the underlying quic-go stream
