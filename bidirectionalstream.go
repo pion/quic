f1f25cd99dfa18974b0c10db59d37c4584875303
Joe Turki <git@joeturki.com>
2025-10-26 17:07:08 +0300

    Change stream to int64

--- bidirectionalstream.go
+++ bidirectionalstream.go
@@ -28,8 +28,8 @@ func (s *BidirectionalStream) ReadInto(data []byte) (StreamReadResult, error) {
 }
 
 // StreamID returns the ID of the QuicStream
-func (s *BidirectionalStream) StreamID() uint64 {
-	return s.s.StreamID()
+func (s *BidirectionalStream) StreamID() StreamID {
+	return StreamID(s.s.StreamID())
 }
 
 // SetDeadline sets read and write deadlines associated with the stream. A zero value for t means Read and Write will not timeout.
