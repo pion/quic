8a490abb4f2e401adc4d318f670acbec7b5b6dce
Pion <59523206+pionbot@users.noreply.github.com>
2026-01-22 08:44:21 +0000

    Update CI configs to v0.11.37
    Update lint scripts and CI configs.

--- internal/wrapper/quic.go
+++ internal/wrapper/quic.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
 // Package wrapper is a wrapper around lucas-clemente/quic-go to match
